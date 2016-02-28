d0dd34602a85fd8fc8ace3ec62507c551295018c
Adam <Adam@Desktop>
2016-02-28 14:58:59 +0100

    test 3

--- SponsorsAndAgencies/src/dk/ms/SponsorsAndAgenciesView/WorldGUIMain.java
+++ SponsorsAndAgencies/src/dk/ms/SponsorsAndAgenciesView/WorldGUIMain.java
@@ -44,7 +44,7 @@ import javax.swing.JCheckBox;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 
-public class WorldGUIMain {							//Adam checks in! Edit 2
+public class WorldGUIMain {							//Adam checks in! Edit 3
 
 	private JFrame frame;
 	private static JTextField Inp_iterations;
